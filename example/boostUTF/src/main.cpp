db3deee83ac1dff1aba6161c6d0ee9c97392c3b5
Moyan Xiao <xiaomoyan@gmail.com>
2015-11-10 00:36:22 +0800

    fix gtest/ BoostUTF need more investigate

--- example/boostUTF/src/main.cpp
+++ example/boostUTF/src/main.cpp
@@ -1,12 +1,19 @@
-#define BOOST_TEST_MODULE master_test_suite_name
-#define	BOOST_TEST_DYN_LINK			[>  <]
+//#define BOOST_TEST_MODULE master_test_suite_name
+//#define	BOOST_TEST_DYN_LINK			
+//#define BOOST_TEST_NO_MAIN
+//#include <boost/test/unit_test.hpp>
+
+//int main(int argc, char* argv[], char* envp[])
+//{
+    //return boost::unit_test::unit_test_main( &init_unit_test, argc, argv );
+//}
+#define BOOST_TEST_MODULE test module name
+#define BOOST_TEST_ALTERNATIVE_INIT_API
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_SUITE(test_suite_name)
-
-BOOST_AUTO_TEST_CASE(test_case_name) {
-	BOOST_MESSAGE( "Boost UTF Hello World" );
-	BOOST_WARN(true);
+// entry point:
+int main(int argc, char* argv[], char* envp[])
+{
+    return boost::unit_test::unit_test_main(init_unit_test, argc, argv);
 }
-BOOST_AUTO_TEST_SUITE_END()
 
---
+++ example/boostUTF/src/test_hello.cpp
@@ -0,0 +1,13 @@
+//#define	BOOST_TEST_DYN_LINK			
+//#include <boost/test/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(test_suite_name)
+
+BOOST_AUTO_TEST_CASE(test_case_name) {
+    BOOST_MESSAGE( "Boost UTF Hello World" );
+    BOOST_WARN(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
